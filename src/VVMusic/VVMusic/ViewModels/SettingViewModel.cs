8b642daa6d89ccbaef2c44bd9164547fad4ecce0
Hong <990211127@qq.com>
2021-12-23 15:18:32 +0800

    Create view model

---
+++ src/VVMusic/VVMusic/ViewModels/SettingViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace VVMusic.ViewModels
+{
+    public class SettingViewModel : VVBaseViewModel
+    {
+        public Command SaveCommand { get; }
+
+        private string serverAddress;
+
+        public string ServerAddress
+        {
+            get { return serverAddress; }
+            set { SetProperty(ref serverAddress, value); }
+        }
+
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { SetProperty(ref userName, value); }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+    }
+}
--- src/VVMusic/VVMusic/Views/SettingPage.xaml.cs
+++ src/VVMusic/VVMusic/Views/SettingPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using VVMusic.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,11 @@ namespace VVMusic.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingPage : ContentPage
     {
+        SettingViewModel settingViewModel = new SettingViewModel();
         public SettingPage()
         {
             InitializeComponent();
+            this.BindingContext = settingViewModel;
         }
     }
 }
